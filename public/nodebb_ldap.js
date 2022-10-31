c3ba8c6d91f9415fe424672f8ba92c1a5b328f89
Barış Soner Uşaklı <barisusakli@gmail.com>
2022-10-31 17:32:46 -0400

    alert fix

--- public/nodebb_ldap.js
+++ public/nodebb_ldap.js
@@ -1,22 +1,23 @@
 'use strict';
 
 define('admin/plugins/nodebb_ldap', ['settings', 'alerts'], function(Settings, alerts) {
-    var ACP = {};
+	var ACP = {};
 
-    ACP.init = function() {
-        Settings.load('nodebbldap', $('.ldap-settings'));
-        $('#save').on('click', function() {
-            Settings.save('nodebbldap', $('.ldap-settings'), function() {
-                alerts.success({
-                    title: 'Settings Saved',
-                    message: 'Please reload your NodeBB to apply these settings',
+	ACP.init = function() {
+		Settings.load('nodebbldap', $('.ldap-settings'));
+		$('#save').on('click', function() {
+			Settings.save('nodebbldap', $('.ldap-settings'), function() {
+				alerts.alert({
+					type: 'success',
+					title: 'Settings Saved',
+					message: 'Please reload your NodeBB to apply these settings',
 					clickfn: function () {
-                        socket.emit('admin.reload');
+						socket.emit('admin.reload');
 					},
-                });
-            });
-        });
-    };
+				});
+			});
+		});
+	};
 
-    return ACP;
+	return ACP;
 });
--- templates/admin/plugins/nodebb_ldap.tpl
+++ templates/admin/plugins/nodebb_ldap.tpl
@@ -130,8 +130,3 @@
     </div>
   </div>
 </form>
-
-<button id="save" class="floating-button mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
-	<i class="material-icons">save</i>
-</button>
-
